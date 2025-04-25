228c8d392eb7918795d3af22d4d91019d9f05f82
xMANUX21 <manuelxd53@gmail.com>
2025-04-24 23:13:02 -0500

    fix: :poop: Se corrige un modulo

--- javascript/botones.js
+++ javascript/botones.js
@@ -1,10 +1,23 @@
-export const crearBotones = (setOperacion) => {
-    const botones = document.querySelectorAll("button[data-valor]");
-    
+export function configurarBotones(pantalla) {
+    const botones = document.querySelectorAll("button[data-valor]")
+    let operacion = ""
+  
+    function actualizarPantalla() { // funcion para limpiar o poner datos en pantalla
+      pantalla.textContent = operacion || "0" // actualiza la pantalla a la operacion o 0
+    }
+  
     botones.forEach(boton => {
       boton.addEventListener("click", () => {
-        const valor = boton.getAttribute("data-valor");
-        setOperacion((operacionActual) => operacionActual + valor);
-      });
-    });
-  };
+        operacion += boton.getAttribute("data-valor")
+        actualizarPantalla()
+      })
+    })
+  
+    return {
+      getOperacion: () => operacion, // devuelve el valor actual
+      setOperacion: (nuevaOperacion) => { // actualiza la operacion
+        operacion = nuevaOperacion
+        actualizarPantalla()
+      }
+    }
+  }
